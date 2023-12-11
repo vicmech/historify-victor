cd70326c70fa8e45a30c2d488db6f253208ae93e
maruseron <42821533+maruseron@users.noreply.github.com>
2023-12-11 00:42:17 -0400

    change generate_tables.sql
    instances of "consultorio" changed for "historify"

--- sql/generate_tables.sql
+++ sql/generate_tables.sql
@@ -1,11 +1,11 @@
 # TODO: integrate to project for next release as a physician foreign key
-#CREATE TABLE IF NOT EXISTS consultorio.specialties (
+#CREATE TABLE IF NOT EXISTS historify.specialties (
 #  id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 #  title varchar(45) NOT NULL,
 #  KEY title_idx (title)
 #) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS consultorio.physicians (
+CREATE TABLE IF NOT EXISTS historify.physicians (
   id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   fname varchar(100) NOT NULL,
   lname varchar(100) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS consultorio.physicians (
   specialty varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS consultorio.patients (
+CREATE TABLE IF NOT EXISTS historify.patients (
   id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_document varchar(8) NOT NULL,
   UNIQUE KEY id_document_UNIQUE (id_document),
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS consultorio.patients (
   phone varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS consultorio.consultations (
+CREATE TABLE IF NOT EXISTS historify.consultations (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   consultation_date timestamp NOT NULL,
   consultation_desc varchar(255) NOT NULL,
@@ -40,6 +40,6 @@ CREATE TABLE IF NOT EXISTS consultorio.consultations (
   patient_id int unsigned NOT NULL,
   physician_id int unsigned NOT NULL,
   
-  FOREIGN KEY (physician_id) REFERENCES consultorio.physicians (id),
-  FOREIGN KEY (patient_id) REFERENCES consultorio.patients (id)
+  FOREIGN KEY (physician_id) REFERENCES historify.physicians (id),
+  FOREIGN KEY (patient_id) REFERENCES historify.patients (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
